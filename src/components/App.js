2a6a6e952e7464fc4e04f2e2a2fe79ad8de4f6ca
unknown <sbcfw@umkc.edu>
2022-04-10 16:24:45 -0500

    updating page formatting, added footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import logo from '../logo.svg';
+import { Container, Row, Col } from 'react-bootstrap'
 import '../styleSheets/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -7,17 +9,31 @@ function App() {
       <header className="App-header">
         <h1>Alzheimer's Detection Application</h1>
       </header>
-      <div className="App-desc">
-        <div className="content-box">
-          <p>Welcome to Team Artemis' Alzheimer's Detection Application! To get started, click on the button on the right.</p>
-        </div>
-      </div>
 
-      <div className="App-get-started">
-        <div className="content-box">
-          <button className="btn">Get Started</button>
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="App-desc">
+              <div className="content-box">
+                <p>Welcome to Team Artemis' Alzheimer's Detection Application!
+                  To get started, click on the button on the right.</p>
+              </div>
+            </div>
+          </Col>
+          
+          <Col>
+            <div className="App-get-started">
+              <div className="content-box">
+                <button className="btn btn-primary">Get Started</button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      <footer>
+        <text className = "appFooter">Courtesy of Team Artemis</text>
+      </footer>
     </div>
   );
 }
