10c56b709a2990231cdb7a8e208bbd25fbc0ab29
Christian Chapman <cachapman2019@gmail.com>
2022-04-13 00:20:09 -0500

    working on flask

--- app.py
+++ app.py
@@ -1,12 +1,96 @@
 # Flask Server
+import flask
+import pandas as pd
+import tensorflow as tf
+import keras
+from keras.models import load_model
+from flask import request
+from jinja2.utils import markupsafe
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from scipy import ndimage
+import os
 
-from flask import Flask
-from flask import jsonify
-import tensorflow.keras as keras
+# instantiate flask 
+app = flask.Flask(__name__)
 
-app = Flask(__name__)
+def universal_preprocess(img):
+    
+    if len(img.shape) != 3:
+        return resize_img_incorrect(img).reshape(1,128,128,1)
+    
+    elif(img.shape[2] != 1):
+        img = rgb2gray(img)
+        return resize_img_incorrect(img).reshape(1,128, 128, 1)
+    
+    else:
+        return resize_img_correct(img).reshape(1,128,128,1)
 
-@app.route('/cnn-model')
-def get_model_evaluation():
+def resize_img_incorrect(img):
 
-    return jsonify(prediction="Likely to have Alzheimer's")
+    desired_width = 128
+    desired_height = 128
+    # Get current depth
+    
+    current_width = img.shape[0]
+    current_height = img.shape[1]
+    # Compute depth factor
+
+    width = current_width / desired_width
+    height = current_height / desired_height
+    width_factor = 1 / width
+    height_factor = 1 / height
+
+    img = ndimage.zoom(img, (width_factor, height_factor), order=1)
+    return img
+
+
+def resize_img_correct(img):
+
+    desired_width = 128
+    desired_height = 128
+    # Get current depth
+    
+    current_width = img.shape[0]
+    current_height = img.shape[1]
+    # Compute depth factor
+
+    width = current_width / desired_width
+    height = current_height / desired_height
+    width_factor = 1 / width
+    height_factor = 1 / height
+
+    img = ndimage.zoom(img, (width_factor, height_factor, 1), order=1)
+    return img
+
+
+
+def rgb2gray(rgb):
+
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray.reshape(gray.shape[0], gray.shape[1])
+
+def full_stack(img):
+    model = keras.models.load_model("C://Users//cchap//source//repos//CS 490 Alzheimers Project//490TermProject//model files//model files//new_model.h5")
+    img = np.array(Image.open(img))
+    img = universal_preprocess(img)
+    if(model.predict(img) > 0.5):
+        return "Signs of Dementia"
+    else:
+        return "No Signs of Dementia"
+
+    
+    
+@app.route('/predict', methods=['GET','POST'])
+def upload():
+    data = request.files['image']
+    results = full_stack(data)
+    print(results)
+    return results
+
+
+# start the flask app, allow remote connections 
+app.run(host='0.0.0.0')
--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import '../styleSheets/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as tf from '@tensorflow/tfjs';
 import { tensor } from '@tensorflow/tfjs';
+import axios from 'axios';
 
 function App() {
 
@@ -16,46 +17,24 @@ function App() {
 
   const [prediction, setPrediction] = useState("Upload an image");
 
-/*
-  useEffect(() => {
-    fetch('/cnn-model').then(res => res.json()).then(data => {
-      setPrediction(data.prediction);
-      
-    });
-    console.log("beans is good");
-  });*/
+
 
   const callFlask = () => {
-    /*
-    fetch('/cnn-model').then(res => console.log(res)JSON.parse(res)).then(data => {
-      setPrediction(data.prediction);
+
+    // Post -> Flask
+    var imgtag = document.getElementById("output");
+    let formData = new FormData();
+    formData.append("image", selectedFile);
+
+    axios.post('/predict', formData).then(function (response) {
+      setPrediction(response.data);
     });
-
-    console.log("beans is bad");
-    console.log(prediction)*/
-
-    console.log("inside handleGetJson"); 
-    fetch('/cnn-model', { headers : { 'Content-Type': 'application/json', 'Accept': 'application/json' } }).then((response) => response.json()).then((messages) => {
-      setPrediction(messages.prediction)});
-
   }
-
-  const handleGetJson = () => { 
-    
-    
-    }
-
-
-
-  //var imgtag = new Image();
 
 	const changeHandler = (event) => {
 		setSelectedFile(event.target.files[0]);
     var reader = new FileReader();
 
-    
-    //console.log(imgtag.title);
-    //console.log(selectedFile);
     var imgtag = document.getElementById("output");
     imgtag.title = selectedFile.name;
 
@@ -64,27 +43,6 @@ function App() {
     };
 
     reader.readAsDataURL(selectedFile);
-
-    /*
-    var http = require('http-request');
-    var options = {url: imgtag.src};
-    http.get(options, '/image.jpeg', function (error, result) {
-      if (error) {
-          console.error(error);
-      } else {
-          console.log('File downloaded at: ' + result.file);
-      }
-  });*/
-
-    //var tensorArray = tf.browser.fromPixels(imgtag);
-
-    
-    
-    //tensorArray = tensorArray.expandDims();
-    //tensorArray = tf.image.transform(tf.cast(tensorArray, 'float32'), [128, 128, 1]);
-    //tf.image.transform(tf.cast(tf.tensor4d([0, tensorArray]), 'float32'), [128,128,1]);
-
-    //const tensorData = tensorArray.dataSync();
 
     console.log(imgtag);
 
@@ -95,15 +53,6 @@ function App() {
     
 
     var imgtag = document.getElementById("myimage");
-    //imgtag.title = selectedFile.name;
-    //imgtag.src = event.target.results;
-
-    //reader.readAsDataURL(selectedFile);
-    
-    //const tensorArray = tf.browser.fromPixels(imgtag);
-    //console.log(event.target.results);
-    //console.log(tensorArray);
-    //console.log("hello world");
 
 	};
 
